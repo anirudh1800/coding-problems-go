0babed4ca918f95295fa035946b0e7c1626cb156
Anirudh Gali <gali.anirudh@gmail.com>
2019-04-30 21:32:36 -0700

    add beautify

---
+++ beautify-arrange-ii.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(constructArray(10, 9))
+}
+
+func constructArray(n int, k int) []int {
+    
+    ans := make([]int, n)
+    c := 0
+    
+    for v:=1; v < n - k; v++ {
+        ans[c] = v
+        c++
+    }
+    
+    for v:=0; v <=k; v++ {
+        if v%2 ==0 {
+            ans[c] = n - k + v / 2
+        } else {
+            ans[c] = n - v / 2
+        }
+        c++
+    }
+    
+    return ans
+
+}
+
+
